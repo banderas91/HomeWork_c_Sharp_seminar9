ab538592b482aacc13abd1c9f72efcb07c603410
K_A_A <banderas91@mail.ru>
2022-09-18 09:19:51 +0300

    задача 3

---
+++ zadacha_3/Program.cs
@@ -0,0 +1,29 @@
+﻿// /* Задача 3. Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n. */
+
+
+Console.Write("Введите неотрицательное число М: ");
+ int m = Convert.ToInt32(Console.ReadLine());
+
+ Console.Write("Введите неотрицательное число N: ");
+ int n = Convert.ToInt32(Console.ReadLine());
+
+int Akkerman(int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    else
+    {
+        if (n == 0)
+        {
+            return Akkerman(m - 1, 1);
+        }
+        else 
+        {
+            return Akkerman(m - 1, Akkerman(m, n - 1));
+        }
+    }
+}
+Console.Write("Результат вычисления функции Аккермана ====> ");
+Console.WriteLine(Akkerman(m, n));
